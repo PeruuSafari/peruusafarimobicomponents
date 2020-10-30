6a9e38e0ee171674252f97d9b6567d2568c9ec12
Raphael Chaula <raphaelchaula@gmail.com>
2020-10-30 16:41:16 +0300

    Modify - PeruuAvatar -> Add circular avatar

--- lib/src/widgets/peruuavatar.dart
+++ lib/src/widgets/peruuavatar.dart
@@ -1,49 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-enum AvatarPlaceholder { Bus, User }
-
-Map<AvatarPlaceholder, String> placeholders = {
-  AvatarPlaceholder.Bus: 'assets/images/bus.png',
-  AvatarPlaceholder.User: 'assets/images/user.png'
-};
-
 class PeruuAvatar extends StatelessWidget {
   final double size;
   final String imageurl;
-  final AvatarPlaceholder placeholder;
+  final String assetimageurl;
 
   const PeruuAvatar({
     Key key,
     this.size,
     @required this.imageurl,
-    @required this.placeholder,
+    @required this.assetimageurl,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ClipOval(
-      child: CachedNetworkImage(
-        imageUrl: imageurl,
-        fit: BoxFit.cover,
-        width: size ?? 36.0,
-        height: size ?? 36.0,
-        placeholder: (context, url) {
-          return Image.asset(
-            placeholders[placeholder],
-            fit: BoxFit.cover,
-            width: size ?? 36.0,
-            height: size ?? 36.0,
-          );
-        },
-        errorWidget: (context, url, error) {
-          return Image.asset(
-            placeholders[placeholder],
-            fit: BoxFit.cover,
-            width: size ?? 36.0,
-            height: size ?? 36.0,
-          );
-        },
+    return CircleAvatar(
+      radius: size / 2 ?? 18.0,
+      backgroundColor: Colors.transparent,
+      child: ClipOval(
+        child: CachedNetworkImage(
+          imageUrl: imageurl,
+          fit: BoxFit.cover,
+          width: size ?? 36.0,
+          height: size ?? 36.0,
+          placeholder: (context, url) {
+            return Image.asset(
+              assetimageurl,
+              fit: BoxFit.cover,
+              width: size ?? 36.0,
+              height: size ?? 36.0,
+            );
+          },
+          errorWidget: (context, url, error) {
+            return Image.asset(
+              assetimageurl,
+              fit: BoxFit.cover,
+              width: size ?? 36.0,
+              height: size ?? 36.0,
+            );
+          },
+        ),
       ),
     );
   }
